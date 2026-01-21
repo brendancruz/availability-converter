7ce6f756c402c947ce42ecd9fd09e6d7a55208a3
brendancruz <brendanmcruz@gmail.com>
2026-01-20 18:18:42 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
-jsimport { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
-  }
+  esbuild: {
+    loader: 'jsx',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 })
